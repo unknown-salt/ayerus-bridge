a97b519bedb32373cf5b8691b9a0518fa7f9a5d6
unknown-symbol <189379653+unknown-symbol@users.noreply.github.com>
2025-08-11 15:13:38 +0200

    feat: multi-bot support

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 import { fetch } from "./fetch";
 
-const bot = "c003e39e77cb4ac3ae2fd3d6e2090731";
+const bots = [
+  "c003e39e77cb4ac3ae2fd3d6e2090731",
+  "693cd916c7084b3c9b1a80f3669f4842",
+];
+
+let names = [];
 
 let regex = new RegExp();
 
 try {
   // prettier-ignore
-  const botName = fetch(`https://api.minecraftservices.com/minecraft/profile/lookup/${bot}`).name;
+  for (const id of bots) {
+    const data = fetch(`https://api.minecraftservices.com/minecraft/profile/lookup/${id}`);
+    names.push(data.name);
+  }
+
+  const botPattern = names.join("|");
+
   // prettier-ignore
-  regex = RegExp(`^(?:.{2,4})?(?:Guild|G) (?:.{2,4})?> (?:\\[.+?\\] )?(?:&7)?(?:${botName}) (?:.{2,4})?\\[(?:.+?)\\](?:.{2,4})?: (?:&r)?(.+?):(?: )?(.*)$`)
-  ChatLib.chat("&7Successfully received bot name!");
+  regex = RegExp(`^(?:.{2,4})?(?:Guild|G) (?:.{2,4})?> (?:\\[.+?\\] )?(?:&7)?(?:${botPattern}) (?:.{2,4})?\\[(?:.+?)\\](?:.{2,4})?: (?:&r)?(.+?):(?: )?(.*)$`)
+
+  ChatLib.chat("&7Successfully received bot names!");
 } catch (err) {
   console.log(err);
-  ChatLib.chat("&cError getting bot name!");
+  ChatLib.chat("&cError getting bot names!");
 }
 
 register("chat", (discord, message = "", event) => {
